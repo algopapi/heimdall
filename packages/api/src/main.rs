ec61b57bd534b99603884f29c7342c4b1370458c
Tarun Kumar <tarunkm__@outlook.com>
2025-07-18 02:48:17 +0530

    api: init with routes

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{
+    get, handler, listener::TcpListener, middleware::Tracing, web::Path, EndpointExt, Route, Server,
+};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {name}")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let api_v1 = Route::new()
+        .at("/transactions", get(transactions))
+        .at("/slots", get(slots))
+        .at("/accounts", get(accounts));
+
+    let app = Route::new()
+        .nest("/api/v1", api_v1)
+        .with(Tracing);
+
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .name("hello-world")
+        .run(app)
+        .await
+}
+
+#[handler]
+fn transactions() -> String {
+    "transactions endpoint".to_string()
+}
+
+#[handler]
+fn slots() -> String {
+    "slots endpoint".to_string()
+}
+
+#[handler]
+fn accounts() -> String {
+    "accounts endpoint".to_string()
 }
