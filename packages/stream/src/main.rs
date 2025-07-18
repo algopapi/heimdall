21292dac28cb1b1a0d2557453c104bff4e6c953d
Tarun Kumar <tarunkm__@outlook.com>
2025-07-18 05:37:12 +0530

    stream: simple calculator example for testing

--- packages/stream/src/main.rs
+++ packages/stream/src/main.rs
@@ -1,17 +1,52 @@
-use dotenv::dotenv;
-use std::env;
+use tonic::transport::Server;
 
-mod utils;
-mod monitors;
-mod feeds;
-mod models;
+use crate::proto::calculator_server::{Calculator, CalculatorServer};
+
+pub mod proto {
+    tonic::include_proto!("calculator");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] = 
+        tonic::include_file_descriptor_set!("calculator_descriptor");
+}
+
+#[derive(Debug, Default)]
+struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self,
+        request: tonic::Request<proto::AddRequest>,
+    ) -> Result<tonic::Response<proto::AddResponse>, tonic::Status> {
+        println!("Got a request: {:?}", request);
+
+        let input = request.get_ref();
+
+        let response = proto::AddResponse {
+            result: input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    dotenv().ok();
-    
-    let endpoint = env::var("GRPC_ENDPOINT")
-        .expect("GRPC_ENDPOINT environment variable must be set");
-    
-    monitors::program_monitor::monitor_program_accounts(endpoint).await
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+
+    let calc = CalculatorService::default();
+
+    let service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()?;
+
+    Server::builder()
+        .accept_http1(true)
+        .layer(tower_http::cors::CorsLayer::permissive())
+        .add_service(service)
+        .add_service(tonic_web::enable(CalculatorServer::new(calc)))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
